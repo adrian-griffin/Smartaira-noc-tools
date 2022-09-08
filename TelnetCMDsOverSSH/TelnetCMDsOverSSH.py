f15197248a33a67f1810ca688e4ced488221a994
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-08 13:09:31 -0600

    Update TelnetCMDsOverSSH.py

--- TelnetCMDsOverSSH/TelnetCMDsOverSSH.py
+++ TelnetCMDsOverSSH/TelnetCMDsOverSSH.py
@@ -80,9 +80,9 @@ stdin.write(str(DESTINATIONUSERNAME)+'''
 stdin.write('''?
 
         ''')
-time.sleep(5)
+time.sleep(15)
 ###!! Reading desired commands from tplinkcommands.txt
-f = open(CWD+'TelnetCMDsOverSSH/tplinkcommands.txt','r')
+f = open(CWD+'tplinkcommands.txt','r')
 command_lines = f.readlines()
 ###!! Passing desired commands into Telnet terminal from STDIN
 for command in command_lines:
