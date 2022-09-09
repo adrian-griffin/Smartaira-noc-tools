75419bdc1ba636876f8b80522f0c3841f4caa436
adrian-griffin <adrianrilay@protonmail.com>
2022-09-08 22:02:24 -0600

    smh

--- smh/smh.py
+++ smh/smh.py
@@ -6,8 +6,17 @@ parser = argparse.ArgumentParser(description="SSH into an Edge using only the pr
 parser.add_argument("PropertyAbbreviation", type=str)
 args = parser.parse_args()
 
-property = args.PropertyAbbreviation
+PROPERTY = args.PropertyAbbreviation
 
-errorNo = subprocess.run('ping -c 1 wea.smartaira360.com',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+test_domain = str(PROPERTY)+".smartaira360.com"
 
-print(errorNo.check_returncode())
+p = subprocess.run( [ 'ping', '-c 1 -s 1 -q', str(test_domain) ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT)
+if int(p.returncode) < 1:
+    VALID_DOMAIN = ".smartaira360.com"
+else: 
+    VALID_DOMAIN = ".bluerim.net"
+
+
+s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+PROPERTY+VALID_DOMAIN+'' ])
