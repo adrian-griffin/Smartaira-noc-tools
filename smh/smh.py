1806182972957c944a5ec113cb47eef87ad580a4
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-11 16:45:06 -0600

    Update smh.py

--- smh/smh.py
+++ smh/smh.py
@@ -7,17 +7,19 @@ parser = argparse.ArgumentParser(description="SSH into an Edge using only the pr
 parser.add_argument("PropertyAbbreviation", type=str)
 args = parser.parse_args()
 
-PROPERTY = args.PropertyAbbreviation
-
+PROPERTY_lower = args.PropertyAbbreviation
+PROPERTY = str(PROPERTY_lower).upper()
 test_domain = str(PROPERTY)+".smartaira360.com"
 
-p = subprocess.run( [ 'ping', '-c 1 -s 1 -q', str(test_domain) ],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.STDOUT)
-if int(p.returncode) < 2:
-    VALID_DOMAIN = PROPERTY+".smartaira360.com"
-else: 
+
+loganEdgeProperties = [
+    'CBS','CCA','DCA','EHA','HSM','KKA','LAB','LOGAN OFFICE','NHA','OFA','OMV','OMVIEW','WRA'
+]
+
+if PROPERTY in loganEdgeProperties:
     VALID_DOMAIN = "logan.smartaira360.com"
+else: 
+    VALID_DOMAIN = PROPERTY+".smartaira360.com"
 
 
 s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+VALID_DOMAIN+'' ],
