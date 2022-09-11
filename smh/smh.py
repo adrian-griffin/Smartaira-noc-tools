ac244079c6e700b4313fa3b5c9e97986ee54d04c
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-11 17:40:19 -0600

    Update smh.py

--- smh/smh.py
+++ smh/smh.py
@@ -27,5 +27,3 @@ else:
 s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+VALID_DOMAIN+'' ],
     stdout=subprocess.DEVNULL)
 
-if str(s.stderr) == 'None':
-    print("Unable to connect to property edge!")
