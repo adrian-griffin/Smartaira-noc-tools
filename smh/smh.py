d2d05a607273f28c63f655ac9977cf23224c810c
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-11 16:13:49 -0600

    Update smh.py

--- smh/smh.py
+++ smh/smh.py
@@ -13,7 +13,7 @@ test_domain = str(PROPERTY)+".smartaira360.com"
 p = subprocess.run( [ 'ping', '-c 1 -s 1 -q', str(test_domain) ],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.STDOUT)
-if int(p.returncode) < 1:
+if int(p.returncode) < 2:
     VALID_DOMAIN = ".smartaira360.com"
 else: 
     VALID_DOMAIN = ".bluerim.net"
