77e225a13a5d60ef34217568ca1750c58560349a
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-11 16:21:55 -0600

    Update smh.py

--- smh/smh.py
+++ smh/smh.py
@@ -12,6 +12,6 @@ try:
     s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+PROPERTY+'.smartaira360.com' ])
 except:
     try: 
-        s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+PROPERTY+'.bluerim.net' ])
-    except:
         s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+'logan'+'.smartaira360.com' ])
+    except:
+        s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+PROPERTY+'.bluerim.net' ])
