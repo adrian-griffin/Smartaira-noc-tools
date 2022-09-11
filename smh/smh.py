4beeb73d0f254620c93efe2569d189b054c70b51
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-11 16:18:15 -0600

    Update smh.py

--- smh/smh.py
+++ smh/smh.py
@@ -8,15 +8,7 @@ args = parser.parse_args()
 
 PROPERTY = args.PropertyAbbreviation
 
-test_domain = str(PROPERTY)+".smartaira360.com"
-
-p = subprocess.run( [ 'ping', '-c 1 -s 1 -q', str(test_domain) ],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.STDOUT)
-if int(p.returncode) < 2:
-    VALID_DOMAIN = ".smartaira360.com"
-else: 
-    VALID_DOMAIN = ".bluerim.net"
-
-
-s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+PROPERTY+VALID_DOMAIN+'' ])
+try:
+    s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+PROPERTY+'.smartaira360.com' ])
+except:
+    s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+PROPERTY+'.bluerim.net' ])
