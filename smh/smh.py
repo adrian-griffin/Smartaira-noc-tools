05057c8a2ba40111631f21343265f599d1851267
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-11 17:44:52 -0600

    Update smh.py

--- smh/smh.py
+++ smh/smh.py
@@ -5,21 +5,25 @@ from sys import stdout
 
 parser = argparse.ArgumentParser(description="SSH into an Edge using only the property abbreviation (eg. smh alno)")
 parser.add_argument("PropertyAbbreviation", type=str)
+global args
 args = parser.parse_args()
 
-PROPERTY_lower = args.PropertyAbbreviation
-PROPERTY = str(PROPERTY_lower).upper()
-test_domain = str(PROPERTY)+".smartaira360.com"
+def sshProperty():
+    PROPERTY_lower = args.PropertyAbbreviation
+    PROPERTY = str(PROPERTY_lower).upper()
+    test_domain = str(PROPERTY)+".smartaira360.com"
 
 
-loganEdgeProperties = [
-    'CBS','CCA','DCA','EHA','HSM','KKA','LAB','LOGAN OFFICE','NHA','OFA','OMV','OMVIEW','WRA'
-]
+    loganEdgeProperties = [
+        'CBS','CCA','DCA','EHA','HSM','KKA','LAB','LOGAN OFFICE','NHA','OFA','OMV','OMVIEW','WRA'
+    ]
 
-if PROPERTY in loganEdgeProperties:
-    VALID_DOMAIN = "logan.smartaira360.com"
-else: 
-    VALID_DOMAIN = str(PROPERTY)+".smartaira360.com"
+    if PROPERTY in loganEdgeProperties:
+        VALID_DOMAIN = "logan.smartaira360.com"
+    else: 
+        VALID_DOMAIN = str(PROPERTY)+".smartaira360.com"
 
 
-s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+VALID_DOMAIN+'' ])
+    s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+VALID_DOMAIN+'' ])
+
+sshProperty()
