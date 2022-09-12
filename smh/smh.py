35180a1709cbce52dcee54ddc89083b05c880981
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-12 08:51:55 -0600

    Update smh.py

--- smh/smh.py
+++ smh/smh.py
@@ -1,15 +1,30 @@
 import argparse
+from ast import parse
 import os
 import subprocess
 import webbrowser
 
 parser = argparse.ArgumentParser(description="SSH into an Edge using only the property abbreviation (eg. smh alno)")
-parser.add_argument("PropertyAbbreviation", type=str, help="Property abbreviation (ALNO, WEA, RRR, OPA, OMV, OMView, etc.)")
+parser.add_argument("PropertyAbbreviation", type=str, help="Property abbreviation (ALNO, WEA, LOGANOFFICE, OPA, OMV, OMView, etc.)")
 
-parser.add_argument('--confluence', type=str, help="Open Confluence page for property")
-parser.add_argument('--graphical', type=str, help="Open Edge admin GUI page")
+
+parser.add_argument('-c','--confluence', type=str, help="Open Confluence page for property")
+parser.add_argument('-g','--graphical', type=str, help="Open Edge admin GUI page in webbrowser")
 
 global args
+
+def sshProperty(VALID_DOMAIN):
+    s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+VALID_DOMAIN+'' ])
+
+
+def edgeGUIProperty(VALID_DOMAIN):
+    url = "http://"+str(VALID_DOMAIN)+"/admin"
+    webbrowser.open(url)
+
+
+
+
+
 
 args = parser.parse_args()
 
@@ -19,7 +34,7 @@ PROPERTY = str(PROPERTY_lower).upper()
 
 
 loganEdgeProperties = [
-    'CBS','CCA','DCA','EHA','HSM','KKA','LAB','LOGAN OFFICE','NHA','OFA','OMV','OMVIEW','WRA'
+    'CBS','CCA','DCA','EHA','HSM','KKA','LAB','LOGANOFFICE','NHA','OFA','OMV','OMVIEW','WRA'
 ]
 
 if PROPERTY in loganEdgeProperties:
@@ -27,14 +42,14 @@ if PROPERTY in loganEdgeProperties:
 else: 
     VALID_DOMAIN = str(PROPERTY)+".smartaira360.com"
 
+if parse.graphical:
+    edgeGUIProperty(VALID_DOMAIN)
 
-def sshProperty(VALID_DOMAIN):
-    s = subprocess.run( [ 'ssh', ''+'agriffin'+'@'+VALID_DOMAIN+'' ])
 
 
-def edgeGUIProperty(VALID_DOMAIN):
-    url = "http://"+str(VALID_DOMAIN)+"/admin"
-    webbrowser.open(url)
+
+
+
 
 
 sshProperty(VALID_DOMAIN)
