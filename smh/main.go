369f983a4e37fe9e7b804cabc5b3d1d24b3322b3
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-16 18:49:00 -0600

    1

--- smh/main.go
+++ smh/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	// ssh config
+	hostKeyCallback, err := knownhosts.New("~/.ssh/known_hosts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &ssh.ClientConfig{
+		User: "ubuntu",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("password"),
+		},
+		HostKeyCallback: hostKeyCallback,
+	}
+	// connect ot ssh server
+	conn, err := ssh.Dial("tcp", "wea.smartaira360.com", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 }
