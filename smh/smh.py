ec3199c11e8542150045fd84d2275aeeb9e0dbcf
root <root@Adrian-Work.localdomain>
2022-10-19 14:39:08 -0600

    hi

--- smh/smh.py
+++ smh/smh.py
@@ -13,21 +13,25 @@ parser.add_argument("PropertyAbbreviation", type=str, help="Property abbreviatio
 
 parser.add_argument('-c','--confluence', action="store_true", help="Open Confluence page for property")
 parser.add_argument('-g','--graphical', action="store_true", help="Open Edge admin GUI page in webbrowser")
+parser.add_argument('-s','--socks',action="store_true",help="SSH to dest and establish SOCKS tunnel proxy")
 
 global SSHBOOL
 global args
 
 
-CWD_unsan = os.getcwd()
-CWD = CWD_unsan.replace("\\","/")
+#CWD_unsan = os.getcwd()
+#CWD = CWD_unsan.replace("\\","/")
 
 config = configparser.ConfigParser()
-config.read(CWD+'/NOC_Radius_Credentials.ini')
+#print(CWD)
+config.read('~/Smartaira-noc-tools/smh/NOC_Radius_Credentials.ini')
 config.sections()
 
 SSHBOOL = True
-RADIUS_PASSWORD = config['RADIUS_CREDENTIALS']['RadiusPassword']
-RADIUS_USERNAME = config['RADIUS_CREDENTIALS']['RadiusUsername']
+##RADIUS_USERNAME = config['RADIUS_CREDENTIALS']['RadiusUsername']
+RADIUS_USERNAME = 'agriffin'
+##RADIUS_PASSWORD = config['RADIUS_CREDENTIALS']['RadiusPassword']
+RADIUS_PASSWORD = 'Y*FNDrn8quNQ.Bq-'
 
 
 def sshProperty(VALID_DOMAIN):
@@ -39,7 +43,8 @@ def edgeGUIProperty(VALID_DOMAIN):
     webbrowser.open(url,0,True)    
     os._exit(0)
 
-
+def socksGUIProperty(VALID_DOMAIN):
+    s = subprocess.run(["ssh","-D", "7070", "{}@{}".format(RADIUS_USERNAME, VALID_DOMAIN)])
 
 args = parser.parse_args()
 
@@ -50,6 +55,10 @@ VALID_DOMAIN = str(PROPERTY)+".smartaira360.com"
 
 if args.graphical:
     edgeGUIProperty(VALID_DOMAIN)
+
+if args.socks:
+    socksGUIProperty(VALID_DOMAIN)
+    SSHBOOL = False
 
 if SSHBOOL == True:
     sshProperty(VALID_DOMAIN)
